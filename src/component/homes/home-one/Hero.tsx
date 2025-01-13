02d5f323359e312bcb8cd830450b0d80b211c350
Gaurav Sharma <110299332+attavya@users.noreply.github.com>
2025-01-13 11:46:06 +0530

    Update Hero.tsx

--- src/component/homes/home-one/Hero.tsx
+++ src/component/homes/home-one/Hero.tsx
@@ -1,14 +1,13 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
 
-import hero_thumb from "@/assets/img/update/hero/onconetwork-banner.gif"
-import CountdownClock from "@/ui/CountDownClock"
+import hero_thumb from "@/assets/img/update/hero/onconetwork-banner.gif";
+import CountdownClock from "@/ui/CountDownClock";
 
 const Hero = () => {
    return (
       <div className="hero-wrapper hero-1">
-         <div className="hero-bg-gradient">
-         </div>
+         <div className="hero-bg-gradient"></div>
          <div className="ripple-shape">
             <span className="ripple-1"></span>
             <span className="ripple-2"></span>
@@ -28,47 +27,27 @@ const Hero = () => {
                   <div className="col-lg-9">
                      <h1 className="hero-title">Data That Fights Cancer</h1>
                      <p>Transform the fight against cancer.</p>
-                     <div className="btn-wrap"> 
-                        <Link target="_blank" href="/assets/onconetwork.pdf" className="btn btn-two" >
+                     <div className="btn-wrap">
+                        <Link target="_blank" href="/assets/onconetwork.pdf" className="btn btn-two">
                            White Paper
                         </Link>
                      </div>
                   </div>
-
                </div>
             </div>
 
-            {/*
-            <div className="hero-countdown-wrap">
-               <h2 className="hero-countdown-wrap-title">Pre-Sale</h2>
-               <ul className="skill-feature_list">
-                  <li><span>Value</span> of technology invested</li>
-                  <li><span>Private</span> sale</li>
-                  <li><h4>ICO</h4></li>
-               </ul>
-               <div className="skill-feature">
-                  <div className="progress">
-                     <div className="progress-bar" style={{ width: "80%" }}>
-                     </div>
-                  </div>
-                  <div className="progress-value-max">100 Min $</div>
-               </div>
-               <ul className="skill-feature_list style2">
-                  <li>7.75 Min</li>
-                  <li>1.5 Min</li>
-                  <li>140,000 $ chosen</li>
-               </ul>
-               <div className="banner-countdown-wrap">
-                  <div className="coming-time" data-countdown="2024/8/29">
-                  <CountdownClock />
-                  </div>
-               </div>
+            {/* Responsive and Centered iFrame */}
+            <div className="iframe-container">
+               <iframe
+                  src="https://gempad.app/embed?address=0x933796c017cfA25083b466e4aA647A1A7FAC8161&bgColor=%2312181F&fgColor=%23161D27&hasAnimation=true&hasBanner=true&hasSocialLinks=true&network=Ethereum&padding=30&refer=&responsive=true&saleType=presale&theme=dark&txtColor=%23FFFFFF"
+                  frameBorder="0"
+                  scrolling="no"
+                  allowFullScreen
+               ></iframe>
             </div>
-            */}
-
          </div>
       </div>
-   )
-}
+   );
+};
 
-export default Hero
+export default Hero;
